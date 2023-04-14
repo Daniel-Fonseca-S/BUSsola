63690f67ccffc52a90f6c6269c15d768bd144643
davi.takayama <davi.takayama@kepha.com.br>
2023-04-13 21:03:08 -0300

    configuração para usar typescript

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,18 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          tests: ['./tests/'],
+          "@components": "./src/components",
+        }
+      }
+    ]
+  ]
   return {
     presets: ['babel-preset-expo'],
   };
