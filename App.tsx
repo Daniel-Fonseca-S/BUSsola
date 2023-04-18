f604033238e3641bc2d4ff60fc978d4f9e2b1ea2
davi.takayama <davi.takayama@kepha.com.br>
2023-04-18 19:32:26 -0300

    INFERNO AAAAAAAA ODEIO REACT NATIVE

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './src/views/login';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
