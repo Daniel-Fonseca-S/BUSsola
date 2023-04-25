f0d2891ad6a986f8de26100c6ef5c12c15732fef
davi.takayama <davi.takayama@kepha.com.br>
2023-04-25 00:08:53 -0300

    tela de login

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Menu from "./src/components/menu";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import Login from "src/views/login";
 
 export default function App() {
 	const Drawer = createDrawerNavigator();
@@ -11,6 +12,7 @@ export default function App() {
 		<NavigationContainer>
 			<Drawer.Navigator>
 				<Drawer.Screen name="Menu" component={Menu} />
+				<Drawer.Screen name="login" component={Login} />
 			</Drawer.Navigator>
 			{/* <Stack.Navigator initialRouteName='Menu'>
 				<Stack.Screen name="Menu" component={Menu} options={{ headerShown: false, statusBarHidden: true }} />
