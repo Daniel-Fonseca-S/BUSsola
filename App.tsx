6846f7cefd133b819af67462ed91c7a572a4491a
John Johnson <mundgs@outlook.com>
2023-04-26 22:17:09 -0300

    select-rota feito com sucesso? provavelmente

--- App.tsx
+++ App.tsx
@@ -3,9 +3,10 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Menu from "./src/components/menu";
 import { createDrawerNavigator } from "@react-navigation/drawer";
-import Login from "src/views/login";
-import RecuperarSenha from "src/views/recuperar-senha";
-import NovaSenha from "src/views/nova-senha";
+import Login from "./src/views/login";
+import RecuperarSenha from "./src/views/recuperar-senha";
+import NovaSenha from "./src/views/nova-senha";
+import SelecionarRota from "./src/views/select-rota";
 
 export default function App() {
 	const Drawer = createDrawerNavigator();
@@ -17,6 +18,7 @@ export default function App() {
 				<Drawer.Screen name="login" component={Login} />
 				<Drawer.Screen name="recuperar-senha" component={RecuperarSenha} />
 				<Drawer.Screen name="nova-senha" component={NovaSenha} />
+				<Drawer.Screen name="select-rota" component={SelecionarRota} />
 			</Drawer.Navigator>
 			{/* <Stack.Navigator initialRouteName='Menu'>
 				<Stack.Screen name="Menu" component={Menu} options={{ headerShown: false, statusBarHidden: true }} />
--- src/views/select-rota/index.tsx
+++ src/views/select-rota/index.tsx
@@ -1,45 +1,58 @@
 import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import { Button, Text } from "@react-native-material/core";
+import { Button, Stack, Text } from "@react-native-material/core";
 
-export default function Ir(vai: any) {
+export default function SelecionarRota(vai: any) {
 
     return (
         <View
-            style={styles.rota}>
-            <Text
-                style={styles.lbl}>
-                Rota UTFPR
-            </Text>
-            <Button
-                title={"IR"}
-                color={"#8D28FF"}
-                style={styles.butn}
-                onPress={() => { vai.navigate("rota-1"); }}
-            />
+        style={styles.rota}>
+            <Stack
+                style={styles.tackstack}>
+                <View
+                style={styles.linha}>
+                    <Text
+                        style={styles.lbl}>
+                        Rota UTFPR
+                    </Text>
+                    <Button
+                        title={"IR"}
+                        color={"#8D28FF"}
+                        style={styles.butn}
+                        onPress={() => { vai.navigate("rota-1"); }}
+                    />
+                </View>
 
-            <Text
-                style={styles.lbl}>
-                Rota Entre Bairros 1
-            </Text>
-            <Button
-                title={"IR"}
-                color={"#8D28FF"}
-                style={styles.butn}
-                onPress={() => { vai.navigate("rota-2"); }}
-            />
+                <View
+                style={styles.linha}>
+                    <Text
+                        style={styles.lbl}>
+                        Rota Entre Bairros 1
+                    </Text>
+                    <Button
+                        title={"IR"}
+                        color={"#8D28FF"}
+                        style={styles.butn}
+                        onPress={() => { vai.navigate("rota-2"); }}
+                    />
+                </View>
 
-            <Text
-                style={styles.lbl}>
-                Rota Entre Bairros 2
-            </Text>
-            <Button
-                title={"IR"}
-                color={"#8D28FF"}
-                style={styles.butn}
-                onPress={() => { vai.navigate("rota-3"); }}
-            />
+                <View
+                style={styles.linha}>
+                    <Text
+                        style={styles.lbl}>
+                        Rota Entre Bairros 2
+                    </Text>
+                    <Button
+                        title={"IR"}
+                        color={"#8D28FF"}
+                        style={styles.butn}
+                        onPress={() => { vai.navigate("rota-3"); }}
+                    />
+                </View>
+
+            </Stack>
         </View>
     );
 
@@ -48,19 +61,26 @@ export default function Ir(vai: any) {
 
 const styles = StyleSheet.create({
     rota: {
+        flex: 1,
+        justifyContent: "space-between",
+        alignItems: "center",
+        backgroundColor: "#000",
+        padding: 10,
+    },
+    tackstack: {
+        width: "95%",
+        //alignItems: "center",
+    },
+    linha: {
         flexDirection: "row",
         justifyContent: "space-between",
         alignItems: "center",
-        width: "100%",
-        backgroundColor: "#000",
         padding: 10,
     },
     lbl: {
         color: "#B7B7B7",
-        padding: 10,
     },
     butn: {
-        width: "8%",
-        padding: 10,
+        width: "22.5%",
     }
 });
