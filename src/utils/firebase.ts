51dc008ace2e5647a3ff6eaa759756d6b92385f8
Daniel Fonseca <101329358+Daniel-Fonseca-S@users.noreply.github.com>
2023-05-16 19:16:28 -0300

    Adição da instância de autênticação do Firebase

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,5 +17,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const firebase = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const auth = getAuth(firebase);
 
 export default firebase;
